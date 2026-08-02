80c40fd0c805fe336a43c4554256fe1330809834
wamurombedzi <wadzamurombedzi@gmail.com>
2026-08-02 07:01:18 +0300

    Validation and Error handing done

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,7 +1,7 @@
 const Validator = require('validatorjs');
 const validator = (body, rules, customMessages, callback) => {
   const validation = new Validator(body, rules, customMessages);
-  validation.pass(() => callback(null, true));
+  validation.passes(() => callback(null, true));
   validation.fails(() => callback(validation.errors, false));
 };
 
